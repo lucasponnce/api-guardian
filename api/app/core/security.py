3dbd99af2a76bd3d0ca4b6e2b52ec6cfa7eb3fcb
luucasp912 <luucasponnce75@gmail.com>
2026-07-09 23:18:06 -0300

    endpoints login y register, JWT creado y decodificado: en proceso de terminar

--- api/app/core/security.py
+++ api/app/core/security.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+import os
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +12,23 @@ def hash_password(password: str) -> str:
 # Se usa en el login
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Función para crear el token
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+# Función para decodificar el token
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        sub = payload.get("sub")
+        return sub
+    except jwt.JWTError:
+        return None
--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.database import get_db
+from app.schemas.user import UserCreate, UserOut
+from app.core.security import hash_password, verify_password, create_access_token, decode_access_token
 from app.models import User
+from app.schemas.login import LoginRequest
 
 app = FastAPI(title="API Guardian")
 
@@ -12,3 +15,27 @@ async def root():
 @app.get("/db-check")
 async def db_check(db: Session = Depends(get_db)):
     return {"users_count": db.query(User).count()}
+
+@app.post("/register", response_model=UserOut)
+async def register(data: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.username == data.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="El nombre de usuario ya existe")
+
+    new_user = User(username=data.username, password_hash=hash_password(data.password))
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.post("/login")
+async def login(data: LoginRequest, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == data.username).first()
+
+    if not user or not verify_password(data.password, user.password_hash):
+        raise HTTPException(status_code=401, detail="Usuario o contraseña incorrectos")
+    
+    access_token = create_access_token({"sub": user.username})
+
+    return {"acces_token": access_token, "token_type": "bearer"}
