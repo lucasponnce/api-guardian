81dc33b1651ec9e6f0b3359e7bc4e500f4fcb994
luucasp912 <luucasponnce75@gmail.com>
2026-07-11 19:31:54 -0300

    refactor: refactorizar la lógica de la extracción de datos en una función

--- security_gateway/app/detection/extraction.py
+++ security_gateway/app/detection/extraction.py
@@ -1,23 +1,23 @@
 import pandas as pd
 from app.db.database import engine
 
-df = pd.read_sql("SELECT * FROM request_logs", engine)
+def extract_features():
+    df = pd.read_sql("SELECT * FROM request_logs", engine)
+    # Agregamos una columna booleana que indique si el request falló
+    df["es_fallo"] = df["status_code"] >= 400
 
-# Calculamos la cantidad de requests por cada ip
-requests_por_ip = df.groupby("ip_address").size()
+    # Calculamos la cantidad de requests por cada ip
+    requests_por_ip = df.groupby("ip_address").size()
+    # Calculamos la tasa de fallos por cada ip
+    tasa_fallos_por_ip = df.groupby("ip_address")["es_fallo"].mean()
+    # Calculamos la cantidad de endpoints distintos por ip
+    endpoints_distintos_por_ip = df.groupby("ip_address")["endpoint"].nunique()
 
-# Agregamos una columna booleana que indique si el request falló
-df["es_fallo"] = df["status_code"] >= 400
+    # Hacemos un DataFrame con las 3 características calculadas
+    features = pd.DataFrame({
+        "total_requests": requests_por_ip,
+        "tasa_fallos": tasa_fallos_por_ip,
+        "endpoints_distintos": endpoints_distintos_por_ip
+    })
 
-# Calculamos la tasa de fallos por cada ip
-tasa_fallos_por_ip = df.groupby("ip_address")["es_fallo"].mean()
-
-# Calculamos la cantidad de endpoints distintos por ip
-endpoints_distintos_por_ip = df.groupby("ip_address")["endpoint"].nunique()
-
-features = pd.DataFrame({
-    "total_requests": requests_por_ip,
-    "tasa_fallos": tasa_fallos_por_ip,
-    "total_endpoints": endpoints_distintos_por_ip
-})
-print(features)
+    return features
